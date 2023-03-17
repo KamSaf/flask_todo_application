a036607120edb9f5087ac521a2362750526c2aa2
bopaf <116653905+bopaf@users.noreply.github.com>
2023-03-17 12:09:48 +0100

    changed project name, added data base, learning new things like adding url

--- Hello.py
+++ Hello.py
@@ -1,6 +1,28 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.app_context()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
 @app.route('/')
-def hello_world():
-   return "Hello World"
+def index():
+    #show all todos
+    todo_list = Todo.query.all()
+    return render_template('base.html', todo_list)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+
+    new_todo = Todo(title="todo 1", complete=False)
+    with app.app_context():
+        db.session.add(new_todo)
+        db.session.commit()
+    app.run(debug=True)
